9dd2df1e1d4926a52b62dd6d2650ec7c15907748
Kadu <kadu@unibr.com>
2021-02-01 16:31:22 -0500

    Criação da Página Kadu

---
+++ pages/kadu.js
@@ -0,0 +1,12 @@
+import Template from "./components/template.js"
+
+function Kadu(){
+    return (<div>
+        <Template />
+        <main>
+            <h1>Quem é Kadu?</h1>
+        </main>
+    </div>)
+}
+
+export default Sobre
